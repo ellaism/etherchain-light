58d40f17ef4b900209da79cd4ab0b9e43f346f75
ellaismer <31844524+ellaismer@users.noreply.github.com>
2018-03-02 08:31:22 -0500

    Update domain reverse records in cache every hour

--- utils/nameformatter.js
+++ utils/nameformatter.js
@@ -178,16 +178,224 @@ var resolverABI = [
   }
 ];
 
+var ensABI = [
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "node",
+        "type": "bytes32"
+      }
+    ],
+    "name": "resolver",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "node",
+        "type": "bytes32"
+      }
+    ],
+    "name": "owner",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "node",
+        "type": "bytes32"
+      },
+      {
+        "name": "label",
+        "type": "bytes32"
+      },
+      {
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "setSubnodeOwner",
+    "outputs": [],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "node",
+        "type": "bytes32"
+      },
+      {
+        "name": "ttl",
+        "type": "uint64"
+      }
+    ],
+    "name": "setTTL",
+    "outputs": [],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "node",
+        "type": "bytes32"
+      }
+    ],
+    "name": "ttl",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint64"
+      }
+    ],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "node",
+        "type": "bytes32"
+      },
+      {
+        "name": "resolver",
+        "type": "address"
+      }
+    ],
+    "name": "setResolver",
+    "outputs": [],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "node",
+        "type": "bytes32"
+      },
+      {
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "setOwner",
+    "outputs": [],
+    "payable": false,
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "node",
+        "type": "bytes32"
+      },
+      {
+        "indexed": true,
+        "name": "label",
+        "type": "bytes32"
+      },
+      {
+        "indexed": false,
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "NewOwner",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "node",
+        "type": "bytes32"
+      },
+      {
+        "indexed": false,
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "node",
+        "type": "bytes32"
+      },
+      {
+        "indexed": false,
+        "name": "resolver",
+        "type": "address"
+      }
+    ],
+    "name": "NewResolver",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "node",
+        "type": "bytes32"
+      },
+      {
+        "indexed": false,
+        "name": "ttl",
+        "type": "uint64"
+      }
+    ],
+    "name": "NewTTL",
+    "type": "event"
+  }
+];
+
 var Web3 = require('web3');
 
 function nameFormatter(config) {  
   this.conf = config;
   
   this.web3 = new Web3();
-  this.web3.setProvider(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+  this.web3.setProvider(config.provider);
 
   this.reverseContract = this.web3.eth.contract(reverseABI).at("0x268e3C120a46d9fF7e27D05eDC570fE82d8c318D");
   this.resolverContract = this.web3.eth.contract(resolverABI).at("0x632dc20Bd49e96CD9ad525e4FfC70Be6368119f1");
+  this.ensContract = this.web3.eth.contract(ensABI).at("0x518232dd973C321107D28Cb11483b857b9A1E158");
   
   this.resolved = {};
   this.names = {};
@@ -195,11 +403,34 @@ function nameFormatter(config) {
   this.clear = function() {
     this.resolved = {};
     this.names = {};
+
+    var self = this;
+    this.ensContract.NewOwner(null, {
+      fromBlock: "0x0"
+    }, function(err, result) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      self.lookup(result.args.owner);
+    });
+    this.ensContract.Transfer(null, {
+      fromBlock: "0x0"
+    }, function(err, result) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      self.lookup(result.args.owner);
+    });
   }
   
   this.startClear = function() {
+    this.clear();
+
     var self = this;
-    
     setTimeout(function() {
       self.clear();
       self.startClear();
@@ -207,27 +438,33 @@ function nameFormatter(config) {
   }
   
   this.startClear();
+
+  this.lookup = function(address) {
+    if (this.resolved[address]) {
+      return;
+    }
+
+    this.resolved[address] = true;
+
+    var self = this;
+    self.reverseContract.node.call(address, function(err, namehash) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      self.resolverContract.name.call(namehash, function(err, domain) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        self.names[address] = domain;
+      });
+    });
+  }
   
   this.format = function(address) {
-    if (this.resolved[address]) {
-      if (this.names[address]) {
-        return this.names[address];
-      } else {
-        return address;
-      }
-    }
-    
-    var cwd = process.cwd();
-    process.chdir("/tmp");
-    var namehash = this.reverseContract.node.call(address);
-    var domain = this.resolverContract.name.call(namehash);
-    process.chdir(cwd);
-    
-    this.resolved[address] = true;
-    this.names[address] = domain;
-
-    if (domain) {
-      return domain;
+    if (this.names[address]) {
+      return this.names[address];
     } else {
       return address;
     }
