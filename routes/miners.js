6aab26a293a8f231b465e11583d7376976c19b9c
ellaismer <31844524+ellaismer@users.noreply.github.com>
2018-03-11 12:12:48 -0400

    Display miners stats information

---
+++ routes/miners.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+var async = require('async');
+var Web3 = require('web3');
+var fs = require('fs');
+
+function formatMinersTotal(miner) {
+  if (miner.stats) {
+    return miner.stats.minersTotal || "";
+  } else {
+    return "";
+  }
+}
+
+function formatMinersHashrate(miner) {
+  if (miner.stats && miner.stats.hashrate) {
+    var hashrate = miner.stats.hashrate;
+    return (hashrate / 1000000000).toFixed(2) + " GH/s";
+  } else {
+    return "";
+  }
+}
+
+router.get('/', function(req, res, next) {
+  var config = req.app.get('config');
+  var web3 = new Web3();
+  web3.setProvider(config.provider);
+
+  fs.readFile(process.env["MINER"], 'utf8', function(err, data) {
+    if (err) {
+      return callback({name:"NoMinersFound", message: "Chain contains no miners."});
+    }
+
+    var data = JSON.parse(data);
+
+    var miners = [];
+    Object.keys(data.miners).forEach(function(minerKey) {
+      data.miners[minerKey].address = minerKey;
+
+      miners.push(data.miners[minerKey]);
+    });
+
+    miners.sort(function(a, b) {
+      return (b.minedWeek > a.minedWeek) ? 1 : ((a.minedWeek > b.minedWeek) ? -1 : 0);
+    });
+
+    res.render("miners", { miners: miners,
+                           formatMinersTotal: formatMinersTotal,
+                           formatMinersHashrate: formatMinersHashrate });
+  });
+});
+
+module.exports = router;
