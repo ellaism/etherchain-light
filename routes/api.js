32861fb472242e964dd7a9cf081d9c2467101d19
ellaismer <31844524+ellaismer@users.noreply.github.com>
2018-03-16 13:18:01 -0400

    Add API for getting total supply

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var async = require('async');
+var Web3 = require('web3');
+var fs = require('fs');
+
+router.get('/cmc/total', function(req, res, next) {
+  fs.readFile(process.env["TOTAL"], 'utf8', function(err, data) {
+    if (err) {
+      return res.status(500).send('Error when reading total supply.')
+    }
+    
+    res.send(data);
+  });
+});
