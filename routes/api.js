ca4b88636f8240a7ed44ac9e53b9b69b7dc7c59e
ellaismer <31844524+ellaismer@users.noreply.github.com>
2018-05-30 21:50:26 -0400

    Add API endpoint for fetching daily and weekly transaction count

--- routes/api.js
+++ routes/api.js
@@ -15,4 +15,26 @@ router.get('/cmc/total', function(req, res, next) {
   });
 });
 
+router.get('/stats/transactions/daily', function(req, res, next) {
+  fs.readFile(process.env["MINER"], 'utf8', function(err, data) {
+    if (err) {
+      return res.status(500).send('Error when reading miner information.')
+    }
+    
+    var data = JSON.parse(data);
+    res.send(data.transactionsDay);
+  });
+});
+
+router.get('/stats/transactions/weekly', function(req, res, next) {
+  fs.readFile(process.env["MINER"], 'utf8', function(err, data) {
+    if (err) {
+      return res.status(500).send('Error when reading miner information.')
+    }
+    
+    var data = JSON.parse(data);
+    res.send(data.transactionsWeek);
+  });
+});
+
 module.exports = router;
