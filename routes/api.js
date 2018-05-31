fd058977ddb54980e76246605f2d4662213f56dc
ellaismer <31844524+ellaismer@users.noreply.github.com>
2018-05-30 22:26:37 -0400

    Unify transaction stats

--- routes/api.js
+++ routes/api.js
@@ -15,25 +15,17 @@ router.get('/cmc/total', function(req, res, next) {
   });
 });
 
-router.get('/stats/transactions/daily', function(req, res, next) {
+router.get('/stats/transactions', function(req, res, next) {
   fs.readFile(process.env["MINER"], 'utf8', function(err, data) {
     if (err) {
       return res.status(500).send('Error when reading miner information.')
     }
     
     var data = JSON.parse(data);
-    res.send(data.transactionsDay);
-  });
-});
-
-router.get('/stats/transactions/weekly', function(req, res, next) {
-  fs.readFile(process.env["MINER"], 'utf8', function(err, data) {
-    if (err) {
-      return res.status(500).send('Error when reading miner information.')
-    }
-    
-    var data = JSON.parse(data);
-    res.send(data.transactionsWeek);
+    res.send({
+      "daily": data.transactionsDay,
+      "weekly": data.transactionsWeek
+    });
   });
 });
 
